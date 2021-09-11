b163b5f27cb3283a93659f9c88daeb942f2442c3
mahisiva29 <97mahisiva@gmail.com>
2021-09-11 12:20:02 +0530

    sound cloud 2

--- main.js
+++ main.js
@@ -1,17 +1,212 @@
+// SC.initialize({
+//   client_id: '340f063c670272fac27cfa67bffcafc4'
+// });
+//
+// $(document).ready(function() {
+//
+//  SC.stream('/tracks/431444451',function(sound){
+//
+//    $('#start').click(function(e) {
+//              e.preventDefault();
+//              sound.start();
+//            });
+//
+//    $('#stop').click(function(e) {
+//               e.preventDefault();
+//               sound.stop();
+//             });
+//  });
+//
+//
+// });
+
+
+
 SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
 
 $(document).ready(function() {
 
- SC.stream('/tracks/556842441',function(sound){
+ SC.stream('/tracks/1003998049',function(sound){
 
-   $('#start').click(function(e) {
+   $('#start1').click(function(e) {
              e.preventDefault();
              sound.start();
            });
 
-   $('#stop').click(function(e) {
+   $('#stop1').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+            });
+ });
+
+
+});
+
+
+
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+
+ SC.stream('/tracks/258015308',function(sound){
+
+   $('#start2').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+
+   $('#stop2').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+            });
+ });
+
+
+});
+
+
+
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+
+ SC.stream('/tracks/747580165',function(sound){
+
+   $('#start3').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+
+   $('#stop3').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+            });
+ });
+
+
+});
+
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+
+ SC.stream('/tracks/380996297',function(sound){
+
+   $('#start4').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+
+   $('#stop4').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+            });
+ });
+
+
+});
+
+
+// 2nd
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+
+ SC.stream('/tracks/842122354',function(sound){
+
+   $('#start5').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+
+   $('#stop5').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+            });
+ });
+
+
+});
+
+
+
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+
+ SC.stream('/tracks/35894712',function(sound){
+
+   $('#start6').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+
+   $('#stop6').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+            });
+ });
+
+
+});
+
+
+
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+
+ SC.stream('/tracks/112010796',function(sound){
+
+   $('#start7').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+
+   $('#stop7').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+            });
+ });
+
+
+});
+
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+
+ SC.stream('/tracks/49374820',function(sound){
+
+   $('#start8').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+
+   $('#stop8').click(function(e) {
               e.preventDefault();
               sound.stop();
             });
